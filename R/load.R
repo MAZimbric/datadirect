b2bde5c491c3068fc60d19bb2da17125164dfc5d
Madsen Zimbric <mazimbric@gmail.com>
2023-03-13 10:40:49 -0500

    generalize loading

--- R/load.R
+++ R/load.R
@@ -1,53 +1,58 @@
-micro_columns <- list(patient_ID = "PatientID",
+micro_columns <- list(PatientID = "PatientID",
                       encounter = "EncounterID",
                       accn = "ACCESSION_NUMBER",
-                      raw_date_time = "COLLECTION_DATE",
+                      raw_datetime = "COLLECTION_DATE",
                       code = "RESULT_CODE",
                       result = "VALUE",
                       text = "RESULT_COMMENT")
 
-#' Load a csv of microbiology data
+#' Load a csv of datadirect data
 #'
 #' @param culture_file String of path to raw culture results file.
 #' @param pt_file String of path to patient identifier file.
 #' @param lab_key String of path to lab assigned patient identifier/MRN key.
-#' @param columns Named list of columns to select from, default value already set.
+#' @param columns Named list of column names as strings to select from
 #'
 #' @return
 #' @export
 #'
 #' @examples
-micro_load_csv <- function(culture_file,
-                           pt_file,
-                           lab_key = NULL,
-                           columns = micro_columns){
+load_csv <- function(culture_file,
+                     pt_file,
+                     #lab_key = NULL,
+                     columns,
+                     key_columns = list(PatientID = "PatientID",
+                                        MRN = "MRN")){
   ## load environment and data ##
-  raw_data <- read_csv(file = culture_file)
-  pt_key <- read_csv(file = pt_file, colClasses = "character")
+  raw_data <- read.csv(file = culture_file)
+  raw_key <- read.csv(file = pt_file, colClasses = "character") %>%
+
+  pt_key <- raw_key %>%
+    dplyr::select(!!!key_columns)
 
   data <- raw_data %>%
     dplyr::select(!!!columns) %>%
-    dplyr::mutate(date = date(mdy_hm(raw_datetime)),
-           time = hms::as_hms(mdy_hm(raw_datetime)))%>%
     dplyr::left_join(pt_key) %>%
     dplyr::select(-PatientID, -raw_datetime)
 
-  if(!is_null(lab_key)){
-    lab_key <- read_csv(file = lab_key, colClasses = "character")
-    if(is_null(lab_key$MRN))
-      stop("MRN column not found in key_")
-    else{
-      data <- data %>%
-        dplyr::left_join(lab_key) %>%
-        select(-MRN)
-      ### rename retroid to PatientID
-    }
-  }
-
-  ## standardize patient identifier column
-  else{
-    data <- data %>%
-      dplyr::rename(PatientID = MRN)
-  }
+  # if(!is_null(lab_key)){
+  #   lab_key <- read_csv(file = lab_key, colClasses = "character")
+  #   if(is_null(lab_key$MRN))
+  #     stop("MRN column not found in key_")
+  #   else{
+  #     data <- data %>%
+  #       dplyr::left_join(lab_key) %>%
+  #       select(-MRN)
+  #     ### rename retroid to PatientID
+  #   }
+  # }
+  #
+  # ## standardize patient identifier column
+  # else{
+  #   data <- data %>%
+  #     dplyr::rename(PatientID = MRN)
+  # }
   return(data)
 }
+
+
